d07f696d1f6d1e061c55e20965d353ef23b627fd
Grawter <terminator17067@mail.ru>
2020-08-10 18:35:02 +0300

    Добавления возможность залогиниться и выйти "личного кабинета"

--- SimpleStore/Controllers/AccountController.cs
+++ SimpleStore/Controllers/AccountController.cs
@@ -46,6 +46,7 @@ namespace SimpleStore.Controllers
             }
             return View(model);
         }
+        
         [AcceptVerbs("Get", "Post")]
         public async Task<IActionResult> CheckEmail(string Email)
         {
@@ -56,5 +57,47 @@ namespace SimpleStore.Controllers
            
             return Json(true);
         }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    // проверка, принадлежит ли URL приложению
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            // удаляем аутентификационные куки
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
