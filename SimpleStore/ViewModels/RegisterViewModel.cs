0d80fc29b54f1dc1ad65f65d60e0497a553bd4b6
Grawter <terminator17067@mail.ru>
2020-08-10 11:58:58 +0300

    Добавления регистрация. Хромает валидация

---
+++ SimpleStore/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleStore.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string SurName { get; set; }
+
+        [Required]
+        [Display(Name = "Адрес")]
+        public string Address { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage = "Некорректный телефон")]
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "День рождения")]
+        public int Day { get; set; }
+
+        [Required]
+        [Display(Name = "Месяц рождения")]
+        public int Mount { get; set; }
+
+        [Required]
+        [Display(Name = "Год рождения")]
+        public int Year { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
